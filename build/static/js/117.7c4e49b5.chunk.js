f2b96ef192701a133898b65cdff9bf255d75346d
sepezho <sepezho@gmail.com>
2022-08-04 17:05:15 +0200

    a

--- build/static/js/117.7c4e49b5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktonana_landing=self.webpackChunktonana_landing||[]).push([[117],{2701:function(n,a,e){e(2791);a.Z=e.p+"static/media/tonana.292167ef1ce505220004959036df9531.svg"}}]);
-//# sourceMappingURL=117.7c4e49b5.chunk.js.map
