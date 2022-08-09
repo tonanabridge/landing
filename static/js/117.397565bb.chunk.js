e6b2ab2be8fd52366389c9a424c089662870a1e4
eanisei <eanisei@users.noreply.github.com>
2022-08-09 14:37:35 +0000

    deploy: fe8152bbb311115ce652ae8293b99df3527eeaf5

--- static/js/117.397565bb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktonana_landing=self.webpackChunktonana_landing||[]).push([[117],{2701:function(a,n,e){e(2791);n.Z=e.p+"static/media/tonana.3e0f5364a1619eb1248bc04f0459a44d.svg"}}]);
-//# sourceMappingURL=117.397565bb.chunk.js.map
